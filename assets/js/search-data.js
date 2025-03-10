babd31557de465f1e53a4dad2e8d8030bb64664d
Jeremalloch <jeremalloch@gmail.com>
2025-03-10 22:55:50 +0000

    Deploying to gh-pages from @ Jeremalloch/Jeremalloch.github.io@dc86b3dac5a78e1aa4d96214e86a1c3e674892e9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Machine Learning Engineer specializing in perception systems for autonomous vehicles at Cruise. Based in San Francisco.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
